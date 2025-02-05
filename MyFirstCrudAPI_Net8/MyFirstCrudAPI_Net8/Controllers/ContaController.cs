e696db2740ab4b3ad900d5905770973185eb3bd9
Leonildo Mazenda <leonildo.mazenda@hotmail.com>
2025-02-05 20:00:22 +0100

    tratndo dac o

--- MyFirstCrudAPI_Net8/MyFirstCrudAPI_Net8/Controllers/ContaController.cs
+++ MyFirstCrudAPI_Net8/MyFirstCrudAPI_Net8/Controllers/ContaController.cs
@@ -2,9 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyFirstCrudAPI_Net8.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+// [Authorize] // Exige autenticação JWT 
 public class ContaController : Controller
 {
-    // GET
+     private readonly ContaSer _depositoService;
+
+        public ContaController(DepositoService depositoService)
+        {
+            _depositoService = depositoService;
+        }
+
     public IActionResult Index()
     {
         return View();
